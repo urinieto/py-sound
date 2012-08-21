2b5502eb2ec6003d833bb4b8ac54edd80cae73a2
Leif Johnson <leif@leifjohnson.net>
2012-08-21 11:39:40 -0500

    Protect README-reading with an except block, since setuptools does not include *.md by default. :(

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
+import os
 import setuptools
+
+# take description from README.md
+here = os.path.dirname(os.path.abspath(__file__))
+readme = ''
+try:
+    readme = file(os.path.join(here, 'README.md')).read()
+except (OSError, IOError):
+    pass
 
 setuptools.setup(
     name='lmj.sound',
-    version='0.1',
+    version='0.1.1',
     install_requires=['scipy', 'scikits.audiolab', 'scikits.samplerate'],
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
     description='An assemblage of code for manipulating sound data',
-    long_description=open('README.md').read(),
+    long_description=readme,
     license='MIT',
     keywords=('sound '
               'fft '
