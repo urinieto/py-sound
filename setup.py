387ede8262e5bd32456775dfc8293af2a931d82f
Leif Johnson <leif@leifjohnson.net>
2012-12-12 15:58:28 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='lmj.sound',
-    version='0.1.2',
+    version='0.1.3',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
